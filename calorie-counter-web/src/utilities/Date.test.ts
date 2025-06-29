54a90167dad0c0f155ee84cbe8427ba3fb289236
btate712 <btate712@gmail.com>
2025-06-29 17:16:09 -0500

    Fix test that was used to verfiy pipeline failure on test failure

--- calorie-counter-web/src/utilities/Date.test.ts
+++ calorie-counter-web/src/utilities/Date.test.ts
@@ -3,6 +3,6 @@ import {Date} from "./Date";
 describe ("Date", () => {
     it("should return a string", () => {
         const date = new Date(2020, 1, 2);
-        expect(date.asString()).toBe("2/2/2020");
+        expect(date.asString()).toBe("1/2/2020");
     })
 })
